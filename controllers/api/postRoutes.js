adef95b45b4843d1e4cdd064349218b1036264f0
JASaddington <jasaddington@gmail.com>
2023-05-21 18:35:25 -0500

    still having issues with dahshboard

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -67,6 +67,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+
 router.delete('/:id', withAuth, async (req, res) =>{
 try {
   const postData = await Post.destroy({
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {Post, User, Comment} = require('../models');
+const withAuth = require('../utils/auth');
 
 
 // GET homepage
@@ -47,9 +48,29 @@ router.get('/', async (req, res) => {
   }  
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+
+  }
+});
+
 router.get('/post/:id', async (req,res) => {
   try{
-    const postData = await Post.findOne(req.params.id, {
+    const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: Comment,
